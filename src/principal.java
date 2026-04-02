43e7af9960303bebc67d24234502fa9f55da5a8b
ClaraKethurin <clarakethurin@gmail.com>
2026-04-02 16:35:55 -0300

    Herança e sobrescreve

---
+++ src/principal.java
@@ -0,0 +1,32 @@
+import br.com.alurascreenmatch.modelos.Filme;
+import br.com.alurascreenmatch.modelos.Serie;
+
+public class principal {
+    static void main() {
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacoes());
+        System.out.println("Total da média de avaliações: " + meuFilme.pegaMedia());
+
+        Serie lost = new Serie();
+        lost.setNome("Lost");
+        lost.setAnoDeLancamento(2000);
+        lost.exibeFichaTecnica();
+        lost.setTemporadas(15);
+        lost.setEpisodiosPorTemporada(10);
+        lost.setMinutosPorEpisodio(160);
+        System.out.println("Duração da série: " + lost.getDuracaoEmMinutos());
+
+    }
+
+
+}
+
+
